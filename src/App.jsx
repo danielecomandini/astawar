4a70bc611a9ca403a07ef0c188ade4b000f20888
danielecomandini <daniele_comandini@hotmail.com>
2025-03-21 22:42:45 +0000

    Pulizia Codice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import Subscriptions from "./components/subscriptions/Subscriptions"
-
-const PLANS = [
-  { plan: 'BASIC', price: 4.99 },
-  { plan: 'PREMIUM', price: 7.99 },
-  { plan: 'MANAGER', price: 9.99 },
-]
 
 const App = () => {
   return <div>ASTA WAR</div>
