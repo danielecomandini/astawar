539709fb590bf6c2e9541e09ec1d5498cf9c9b6b
danielecomandini <daniele_comandini@hotmail.com>
2025-04-15 13:57:17 +0100

    Button Menu

--- src/pages/home/components/AstaWarHomeNavigationBar.jsx
+++ src/pages/home/components/AstaWarHomeNavigationBar.jsx
@@ -1,6 +1,8 @@
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
 import AW_Text from "../../../UI/elements/texts/AWText";
+import AW_Icon from "../../../UI/elements/icons/AWIcon";
 import { AW_COLORS } from "../../../UI/themes/AstaWarTheme";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const AstaWarHomeNavigationBar = () => {
   return (
@@ -11,6 +13,15 @@ const AstaWarHomeNavigationBar = () => {
             backgroundColor: AW_COLORS.white,
           }}
         >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <AW_Icon icon={faBars} color={AW_COLORS.green} />
+          </IconButton>
           <AW_Text variant="h3" color={AW_COLORS.green} fontWeight="bold">
             ASTA WAR
           </AW_Text>
