91e79534a23755689f335a9cdbfe6e28041f5544
danielecomandini <daniele_comandini@hotmail.com>
2025-04-16 17:00:55 +0100

    Preparation for the menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header from "./components/Haeder";
+import Menu from "./components/Menu";
 
 const App = () => {
   return (
     <>
       <Header>ASTA WAR</Header>
+      <section>
+        <div style={{ display: "flex" }}>
+          <Menu />
+          <div>CONTENT</div>
+        </div>
+      </section>
     </>
   );
 };
---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Box, Toolbar } from "@mui/material";
+
+const Menu = () => {
+  return (
+    <Box
+      style={{
+        width: "200px",
+      }}
+    >
+      <Toolbar>MENU</Toolbar>
+    </Box>
+  );
+};
+
+export default Menu;
