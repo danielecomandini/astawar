1559cfb57eb7df9ac6ae2b26a01cd58fb6dcb8ab
danielecomandini <daniele_comandini@hotmail.com>
2025-04-18 12:36:46 +0100

    Cleaning import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import AstaWarHeader from "./UI/layouts/AstaWarHeader";
 import AstaWarLayout from "./UI/layouts/AstaWarLayout";
-import AstaWarMenu from "./UI/layouts/AstaWarMenu";
 
 export default function App() {
   return (
