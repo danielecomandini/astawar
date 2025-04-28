281d35464fadfc15be73980c9ffbc39f42cb84cd
danielecomandini <daniele_comandini@hotmail.com>
2025-04-28 12:40:26 +0100

    Layout of the main page with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import AstaWarIndex from "./UI/layout/AstaWarIndex";
+import AstaWarHeader from "./UI/layout/components/AstaWarHeader";
 
 const App = () => {
   return (
     <>
-      <AstaWarIndex />
+      <AstaWarHeader />
+      <Router>
+        <Routes>
+          {/* Public route */}
+          <Route path="/login" element={<div>LOGIN</div>} />
+
+          {/* Protected area with fixed Layout */}
+          <Route path="/" element={<Outlet />}>
+            <Route index element={<AstaWarIndex />} />
+            <Route path="dashboard" element={<div>DASHBOARD</div>} />
+            <Route path="settings" element={<div>SETTINGS</div>} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 };
