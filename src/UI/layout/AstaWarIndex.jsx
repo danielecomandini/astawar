fdfada0c8aebebc3456d0647149794e8e5b56787
danielecomandini <daniele_comandini@hotmail.com>
2025-04-28 11:56:28 +0100

    Cleaning import

--- src/UI/layout/AstaWarIndex.jsx
+++ src/UI/layout/AstaWarIndex.jsx
@@ -1,9 +1,4 @@
 import { Toolbar } from "@mui/material";
-import AWText from "../elements/AWText";
-import { AW_COLORS } from "../themes/AstaWarTheme";
-import AWIcon from "../elements/AWIcon";
-import AWButton from "../elements/AWButton";
-import { MAIN_FUNCTIONALITIES } from "../../assets/objects/MAIN_FUNCTIONALITIES";
 import AstaWarHeader from "./components/AstaWarHeader";
 import AstaWarUpperSection from "./components/AstaWarUpperSection";
 import AstaWarMainFunctionality from "./components/AstaWarMainFunctionality";
