0b39114ab5d1756809b4aa951a31410944a473d2
danielecomandini <daniele_comandini@hotmail.com>
2025-06-20 11:56:24 +0100

    Menu Review

--- src/assets/menu/MenuDefinition.js
+++ src/assets/menu/MenuDefinition.js
@@ -1,13 +1,14 @@
 import {
   faCalendar,
   faChartArea,
+  faChartLine,
   faEuro,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 
 export const MENU_DEFINITION = [
-  { id: "dashboard", label: "Dashboard", icon: faChartArea },
-  { id: "calendar", label: "Calendar", icon: faCalendar },
+  { id: "calendar", label: "Le Mie Aste", icon: faCalendar },
+  { id: "stats", label: "Statistiche", icon: faChartLine },
   { id: "pricing", label: "Pricing", icon: faEuro },
   { id: "profile", label: "Profile", icon: faUser },
 ];
