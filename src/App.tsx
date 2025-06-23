0c4467fb258f5fc579b3ffd5e600eb4ed2cd39d3
danielecomandini <daniele_comandini@hotmail.com>
2025-06-23 11:30:50 +0100

    Definition of /

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./UI/layout/Layout.jsx";
 import { MENU_DEFINITION } from "./assets/menu/MenuDefinition.js";
+import HomeIndex from "./pages/home/HomeIndex.js";
 
 const getComponent = (component: String) => {
   const Component = component;
-  return <Component />
-}
+  return <Component />;
+};
 
 const App = () => {
   const MENU = MENU_DEFINITION;
 
-  
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+          <Route path="/" element={<HomeIndex />} />
           {MENU.map((route) => (
             <Route
               key={route.id}
               path={`/${route.id}`}
-              element={getComponent( route.component )}
+              element={getComponent(route.component)}
             />
           ))}
         </Route>
