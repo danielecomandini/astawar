4a9fea009d4c85d5d7ac58883fb912023d44f8a6
danielecomandini <daniele_comandini@hotmail.com>
2025-06-23 21:33:01 +0100

    Spacing default

--- src/UI/elements/grids/AWRow.jsx
+++ src/UI/elements/grids/AWRow.jsx
@@ -3,7 +3,10 @@ import { Box, Grid } from "@mui/material";
 const AWRow = ({ children, spacing }) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={spacing || 4}>
+      <Grid
+        container
+        spacing={spacing || 4}
+      >
         {children}
       </Grid>
     </Box>
